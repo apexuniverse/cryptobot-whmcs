e07481c5ff731f1c19fe8b9509601aa7678cf3a5
polenikaa <idkhomka@proton.me>
2025-01-02 14:24:13 +0300

    hot fix

--- cryptobot.php
+++ cryptobot.php
@@ -17,9 +17,6 @@ function cryptobot_config() {
 
 function cryptobot_link($params){
     $gatewayParams = getGatewayVariables('cryptobot');
-    if(!$gatewayParams['token']){
-        die('Not all data in the module settings are specified');
-    }
     $ch = curl_init();
     curl_setopt($ch, CURLOPT_URL, "https://pay.crypt.bot/api/createInvoice?currency_type=fiat&fiat=USD&amount=".$params['amount']."&payload=".$params['invoiceid']."&paid_btn_name=callback&paid_btn_url=https://".$_SERVER['HTTP_HOST']);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
